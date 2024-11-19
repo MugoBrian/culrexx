b0bd060a19b432b8631dd0199ad1d42ef6fcb94f
MugoBrian <mikanmuchiri@gmail.com>
2024-11-19 22:10:46 +0300

    Adjusted the margin and font size.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,8 @@
 const Hero = () => {
   return (
-    <div className="mt-20">
+    <div className="mt-20 px-4">
       <div>
-        <h2 className="mb-8 text-center text-white text-5xl line-height font-semibold leading-tight">
+        <h2 className="mb-8 text-center text-white text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold leading-snug md:leading-tight">
           Currencies from your local banks & exchange rate
         </h2>
       </div>
