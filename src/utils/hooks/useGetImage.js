feb141f68a6bf1451e2f858e133ee41b49dee7db
MugoBrian <mikanmuchiri@gmail.com>
2024-11-19 22:16:32 +0300

    added comments.

--- src/utils/hooks/useGetImage.js
+++ src/utils/hooks/useGetImage.js
@@ -1,7 +1,6 @@
 const useGetImage = (flagCode) => {
+    // Create a url for the flags images
+  return new URL(`../../assets/flags/${flagCode}.png`, import.meta.url).href;
+};
 
-return new URL(`../../assets/flags/${flagCode}.png`, import.meta.url).href
-  
-}
-
-export default useGetImage
+export default useGetImage;
