95978771f9edd8b5858d3a16f90c0c53a49520b0
MugoBrian <mikanmuchiri@gmail.com>
2024-12-03 03:38:20 +0300

    Added margin bottom for spacing. Removed unnecessary jsx brackets.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,9 @@ import { FaHeart } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
+    <div className="mb-8">
       <p className="flex items-center justify-center">
-        &copy; 2024 All rights reserved. Made with{" "}
+        &copy; 2024 All rights reserved. Made with 
         <FaHeart className="text-[#521516] ml-2" />
       </p>
     </div>
